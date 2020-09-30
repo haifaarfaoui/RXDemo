e8b625cfdd3e003dbadf3909fc57737d07f07c7f
haifa <h.arfaoui@mintit.io>
2020-09-30 12:59:22 +0100

    Initial Commit

---
+++ RXDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RXDemo
+//
+//  Created by Mint on 9/30/20.
+//  Copyright © 2020 Mint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
